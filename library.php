af9d91dbf9acae2a883e7fc65b5ead9d42f0ce3b
manal kh <khalqallahmanal@gmail.com>
2025-04-08 17:45:14 +0200

    manal 08/04

---
+++ library.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<!-- ce DOCTYPE est nécessaire avec IE pour les marges automatiques -->
+<html>
+<head>
+<title>Library</title>
+ <meta NAME="Author" CONTENT="M">
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+ <link href="style_op.css" type="text/css" rel="stylesheet" media="all">
+ </head>
+ <body>
+
+ <div class="header2">
+<div class="logo">
+<a href="index.html">
+    <img class="logo" src="logo.png">
+ </a>
+  </div>
+  <h1>LIBRARY</h1>
+</div>
+
+<div class=container>
+	<h4>WANT TO READ</h4>
+	<img class="image" src="imag1.png" alt="image1">
+</div>
+
+<div class=container>
+	<h4>READING</h4>
+	<img class="image" src="imag2.png" alt="image2">
+</div>
+
+<div class=container>
+	<h4>ALREADY READ</h4>
+	<img class="image" src="imag3.png" alt="image3">
+</div>
+
+
+
+<ul>
+    <a href='index.html' class="back">retour accueil</a>
+</ul>
+</body>
+</html>
+
--- search.php
+++ search.php
@@ -1,14 +1,13 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Mon annuaire en PHP</title>
+<title>SEARCH</title>
  <meta NAME="Author" CONTENT="M">
  <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
-  <!-- appel feuille de style -->
  <link href="style_op.css" type="text/css" rel="stylesheet" media="all">
 </head>
 <body>
-<h1>Rechercher un nom</h1>
+<h1>Search for a webtoon</h1>
 <table>
 <form method='POST' action='vue_cherche_par_nom.php' name='annuaire' enctype='application/x-www-form-urlencoded'>
 <tr>
